5e8b3c732bc3b3183a8a55f34e59e014a9227682
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-10-27 13:35:11 +0500

    make changes in the app component

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const Playlist1: VideoComponentType[] = [
   {
     id: 3,
     thumbnail: "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
-    title: "Why Gratitude and Patience Can Transform Your Entire Life",
+    title: "Even Shadows Have Names When the Sun Refuses to Set Completely",
     views: "11.8",
     createdDate: "1"
   },
@@ -46,21 +46,21 @@ const Playlist2: VideoComponentType[] = [
   {
     id: 6,
     thumbnail: "https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=500",
-    title: "My Complete Morning Routine for a Productive and Peaceful Day",
+    title: "The Day the Mountains Moved, We Learned What Silence Really Sounds Like",
     views: "5.8",
     createdDate: "19"
   },
   {
     id: 7,
     thumbnail: "https://images.unsplash.com/photo-1760669347025-dcceae4c8841?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDd8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=60&w=500",
-    title: "Why Gratitude and Patience Can Transform Your Entire Life",
+    title: "The River Carried Secrets No One Was Brave Enough to Remember Forever",
     views: "11.8",
     createdDate: "1"
   },
    {
     id: 8,
     thumbnail: "https://plus.unsplash.com/premium_photo-1756236543159-c49f8adb9dda?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&q=60&w=500",
-    title: "Why Gratitude and Patience Can Transform Your Entire Life",
+    title: "Voices Whisper Softly Through the Walls of Forgotten Dreams and Memories",
     views: "11.8",
     createdDate: "1"
   }
@@ -71,7 +71,7 @@ const Playlist2: VideoComponentType[] = [
       <div className="flex flex-col gap-4">
 
       <PlayListComponent data = {Playlist1} description = "My Favourite Playlist 1" />
-      <PlayListComponent data = {Playlist2} description = "My Favourite Playlist 1"/>
+      <PlayListComponent data = {Playlist2} description = "My Favourite Playlist 2"/>
       </div>
     </>
   )
